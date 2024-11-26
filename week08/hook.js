0c14fba3ccdfab1a00854062d9d8f0508a73fca5
ahyohyo <sywjsx0x@gmail.com>
2024-11-26 21:18:22 +0900

    react hooks

--- week08/hook.js
+++ week08/hook.js
@@ -10,5 +10,4 @@ useMemo(() => {
   }, [])
   /*
     useCallback은 결국 useMemo로 함수를 반환하는 상황에서 더 편하게 사용할 수 있는 Hook이다. 
-
     숫자, 문자열, 객체 처럼 일반 값을 사용 → useMemo를 사용하고, 함수를 재사용 → useCallback을 사용하면 된다.  */
